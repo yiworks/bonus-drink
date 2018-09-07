03adef1b01b226343faf8f74d7d694429341555e
Yuki Ito <y.ito.works@gmail.com>
2018-09-07 19:51:07 +0900

    最初の挑戦

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    bonus_count = 3
+    drank_count = 0
+
+    while amount >= bonus_count
+      drank_count += bonus_count
+      amount -= bonus_count
+      amount += 1
+    end
+
+    amount + drank_count
   end
 end
